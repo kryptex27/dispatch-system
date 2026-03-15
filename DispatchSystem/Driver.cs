730b367e09a2957636fade730700435c6210eb11
Tazo Sazandrishvili <sazandrishvili6@gmail.com>
2026-03-15 08:16:51 +0400

    Add Driver model

---
+++ DispatchSystem/Driver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DispatchSystem
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int UnitNumber { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string LicenseNumber { get; set; }
+
+        public string VehicleType { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public bool Citizen { get; set; }
+
+        public bool CriminalRecord { get; set; }
+
+    }
+}
